ad9ffa4508a4421c217786da4c614720a2f671fc
Phan Dung Tri <phandungtri99@gmail.com>
2020-10-27 10:51:03 +0700

    Real Online Gameplay

--- src/caroserver/Player.java
+++ src/caroserver/Player.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.UUID;
 
 /**
  *
@@ -21,11 +22,25 @@ public class Player implements Runnable {
     private BufferedReader in;
     private BufferedWriter out;
     private String request;
+    private Room room;
+    private String id;
 
     private class OnNewMove implements Command {
         public void execute(String data) {
-            String[] moves = data.split(";");
-            System.out.println(moves[0] + " - " + moves[1]);
+            String[] parts = data.split(";");
+            int col = Integer.parseInt(parts[0]);
+            int row = Integer.parseInt(parts[1]);
+            String fromPlayer = parts[2];
+
+            if (room.getCurrentPlayerId().equals(fromPlayer) && room.newMove(col, row, fromPlayer)) {
+                room.sendAll(request);
+
+                if (room.isWinning(col, row)) {
+                    room.sendAll("END:" + room.getCurrentPlayerId());
+                }
+
+                room.nextTurn();
+            }
         }
     }
 
@@ -34,6 +49,13 @@ public class Player implements Runnable {
         request = "";
         in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
         out = new BufferedWriter(new OutputStreamWriter(this.socket.getOutputStream()));
+        createId();
+    }
+
+    private void createId() throws IOException {
+        id = UUID.randomUUID().toString();
+        out.write("CON:" + id + "\n");
+        out.flush();
     }
 
     private void on(String command, Command handler) {
@@ -43,6 +65,14 @@ public class Player implements Runnable {
         }
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
     public void run() {
         try {
             System.out.println("Client connected!");
@@ -50,6 +80,15 @@ public class Player implements Runnable {
                 request = in.readLine();
                 on("MOV", new OnNewMove());
             }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void response(String data) {
+        try {
+            out.write(data + "\n");
+            out.flush();
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
